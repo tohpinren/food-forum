db7081bb089df327223234aef6d2c0ea504d96cb
Pin Ren <pinren01@gmail.com>
2022-12-23 22:36:31 +0800

    Added single Post show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "posts#index"
 
-  get "/posts", to: "posts#index"
+  resources :posts
   # Defines the root path route ("/")
   # root "articles#index"
 end
