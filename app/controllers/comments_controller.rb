074db5ca5f95a95438a7462c259b3a431c52f43e
Pin Ren <pinren01@gmail.com>
2022-12-24 15:53:48 +0800

    Fixed git rebase

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,25 +1,12 @@
 class CommentsController < ApplicationController
-<<<<<<< HEAD
-  def create
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
-    redirect_to post_path(@post)
-  end
-
-  private
-    def comment_params
-        params.require(:comment).permit(:commenter, :body)
-    end
-=======
     def create
         @post = Post.find(params[:post_id])
         @comment = @post.comments.create(comment_params)
         redirect_to post_path(@post)
       end
     
-      private
+    private
         def comment_params
-          params.require(:comment).permit(:commenter, :body)
+            params.require(:comment).permit(:commenter, :body)
         end
->>>>>>> 4802ee3 (Added comments functionality)
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,3 @@
 class Comment < ApplicationRecord
-<<<<<<< HEAD
-  belongs_to :article
-=======
   belongs_to :post
->>>>>>> 4802ee3 (Added comments functionality)
 end
