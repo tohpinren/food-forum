338596ab16628cd2a3085e294be1b48003ae1dcb
Pin Ren <pinren01@gmail.com>
2022-12-24 16:01:00 +0800

    Delete comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,13 @@ class CommentsController < ApplicationController
         @comment = @post.comments.create(comment_params)
         redirect_to post_path(@post)
       end
+
+    def destroy
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+        @comment.destroy
+        redirect_to post_path(@post), status: :see_other
+    end
     
     private
         def comment_params
