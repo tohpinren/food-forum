5002cc460ee1a23cf9b1ca693a075cc409b04351
Pin Ren <pinren01@gmail.com>
2022-12-24 14:36:57 +0800

    Added delete post functionality (button)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,6 +35,13 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
   private
     def post_params
       params.require(:post).permit(:title, :body)
