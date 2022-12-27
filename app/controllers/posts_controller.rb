5e28820feb4ccf508be8964f1ff9c62a92424360
Pin Ren <pinren01@gmail.com>
2022-12-27 21:22:04 +0800

    Show new post only if logged in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :authorize_user, only: [:edit, :update, :destroy]
 
   def authorize_user
