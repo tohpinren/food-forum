7a1654c869887cc094fecbd2118e7edbf4850ca9
Pin Ren <pinren01@gmail.com>
2022-12-27 13:38:22 +0800

    Added edit comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,11 @@
 class CommentsController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
     def create
         @post = Post.find(params[:post_id])
         @comment = @post.comments.create(comment_params)
         redirect_to post_path(@post)
-      end
+    end
 
     def destroy
         @post = Post.find(params[:post_id])
@@ -11,9 +13,27 @@ class CommentsController < ApplicationController
         @comment.destroy
         redirect_to post_path(@post), status: :see_other
     end
-    
+
+    def edit
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+
+        if @comment.update(comment_params)
+            redirect_to post_path(@post)
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
     private
         def comment_params
-            params.require(:comment).permit(:commenter, :body)
+            params
+                .require(:comment)
+                .permit(:commenter, :body)
         end
 end
