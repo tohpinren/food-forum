dd5d5d76af77812f91c6ae55fe6b9ee98426b820
Pin Ren <pinren01@gmail.com>
2022-12-27 20:11:10 +0800

    Added user_id to post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_27_074002) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_27_120611) do
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_27_074002) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
