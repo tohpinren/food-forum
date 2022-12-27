c8f9fc768d05a1a7efe671127059c50c778e65d1
Pin Ren <pinren01@gmail.com>
2022-12-27 15:46:04 +0800

    Create user model with devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2022_12_24_071157) do
-  create_table "comments", force: :cascade do |t|
-    t.string "commenter"
-    t.text "body"
-    t.integer "article_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["article_id"], name: "index_comments_on_article_id"
-=======
-ActiveRecord::Schema[7.0].define(version: 2022_12_24_074009) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_27_074002) do
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
@@ -28,7 +18,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_24_074009) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
->>>>>>> 4802ee3 (Added comments functionality)
   end
 
   create_table "posts", force: :cascade do |t|
@@ -38,9 +27,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_24_074009) do
     t.datetime "updated_at", null: false
   end
 
-<<<<<<< HEAD
-  add_foreign_key "comments", "articles"
-=======
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   add_foreign_key "comments", "posts"
->>>>>>> 4802ee3 (Added comments functionality)
 end
