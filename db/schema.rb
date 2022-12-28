ec99ba7ab22999f22412ec1d4e27831bf6e1b9d9
Pin Ren <pinren01@gmail.com>
2022-12-28 14:30:18 +0800

    Add category id to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_28_062747) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_28_062841) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -32,6 +32,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_28_062747) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_posts_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -47,4 +49,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_28_062747) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "posts", "categories"
 end
