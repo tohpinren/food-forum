d5acd7c421ca430c09fc525061927861c81d45e5
Pin Ren <pinren01@gmail.com>
2022-12-28 14:28:07 +0800

    Generate Categories model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_28_062701) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_28_062747) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "post_id", null: false
