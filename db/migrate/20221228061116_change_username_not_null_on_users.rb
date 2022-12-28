61f974c056eab24c6b2a9adce2d40c09f3b6a925
Pin Ren <pinren01@gmail.com>
2022-12-28 14:12:19 +0800

    Make username not nullable in database

---
+++ db/migrate/20221228061116_change_username_not_null_on_users.rb
@@ -0,0 +1,4 @@
+class ChangeUsernameNotNullOnUsers < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_28_060424) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_28_061116) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "post_id", null: false
